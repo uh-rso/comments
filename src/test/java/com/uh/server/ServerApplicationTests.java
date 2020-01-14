dd73415849c43d6cb926ad0ab79443c6325cc389
Uroš Hekić <uros.hekic@marand.si>
2020-01-14 15:43:30 +0100

    Make context load again

--- src/test/java/com/uh/server/ServerApplicationTests.java
+++ src/test/java/com/uh/server/ServerApplicationTests.java
@@ -2,9 +2,14 @@ package com.uh.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 @SpringBootTest
 class ServerApplicationTests {
+
+	@MockBean
+	JwtDecoder jwtDecoder;
 
 	@Test
 	void contextLoads() {
